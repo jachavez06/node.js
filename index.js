10b4738fa71aa5aaa2293df1269c33425f08420c
Jesus Chavez <jachavez06@gmail.com>
2017-08-07 21:16:42 -0500

    Post

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
 
 app.use(express.static('public'));
 app.get('/index.htm', function (req, res) {
@@ -10,13 +12,13 @@ app.get('/index.htm', function (req, res) {
 })
 
 app.post('/process_post', urlencodedParser, function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
+    // Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // Listener
