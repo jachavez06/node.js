d01d52bc64f658c68dd8563d675f02a9b3fb88c9
Jesus Chavez <jachavez06@gmail.com>
2017-08-07 21:09:14 -0500

    Homepage can now handle input from form as well as get requests.

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.htm");
+})
 
-// Route GET root requests
-app.get('/', function (req, res) {
-    res.send('Hello GET');
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
 // Listener
