b2617705fcbdcda855e8388e15abb5c34ca4547b
Jesus Chavez <jachavez06@gmail.com>
2017-08-07 21:13:25 -0500

    handle post.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
 app.get('/index.htm', function (req, res) {
     res.sendFile(__dirname + "/" + "index.htm");
 })
 
-app.get('/process_get', function (req, res) {
-    // Prepare output in JSON format
-    response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
 })
 
 // Listener
